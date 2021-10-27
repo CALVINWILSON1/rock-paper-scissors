e112f4b6f139f89d08edf6d645b55b876cd692bf
CALVINWILSON1 <92391915+CALVINWILSON1@users.noreply.github.com>
2021-10-27 04:59:57 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let Tool = 0
+if (Tool == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+} else if (Tool == 1) {
+    basic.showIcon(IconNames.Square)
+} else {
+    basic.showIcon(IconNames.Scissors)
+}
